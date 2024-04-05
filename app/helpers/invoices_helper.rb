dc64e43008f1ef623d63b4035553e6a79f33e472
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-04-05 21:46:20 +0530

    add tom-select is complete, working on this in progress

--- app/helpers/invoices_helper.rb
+++ app/helpers/invoices_helper.rb
@@ -1,2 +1,7 @@
 module InvoicesHelper
+  def categories
+    @categories.map do |category|
+      [category.name, category.products.map { |product| [product.name, product.id] }]
+    end
+  end
 end
--- app/javascript/controllers/invoice_controller.js
+++ app/javascript/controllers/invoice_controller.js
@@ -1,12 +1,50 @@
 import { Controller } from "@hotwired/stimulus"
-import { get, post } from "@rails/request.js"
+import { get, post, destroy } from "@rails/request.js"
+import TomSelect from 'tom-select'
+
 
 export default class extends Controller {
   static targets = ["product", "lineitem"]
 
-  handler() {
+  connect() {
+    let addEventHandler = () => (...args) => this.addHandler(...args);
+    let removeEventHandler = () => (...args) => this.removeHandler(...args);
+    this.select = new TomSelect("#product_id",{
+      plugins: ['remove_button'],
+      create: true,
+      closeAfterSelect: true,
+      onItemAdd: addEventHandler(),
+      onItemRemove: removeEventHandler(),
+      render:{
+        option:function(data,escape){
+          return '<div class="d-flex"><span>' + escape(data.text) + '</span></div>';
+        },
+        item:function(data,escape){
+          console.log(data)
+          return '<div>' + escape(data.text) + '</div>';
+        }
+      }
+    });
+
+    // this.select.on('item_remove', (event) => {
+    //   // console.log(this.element.children[4].id)
+    //   let lineItemId = this.element.children[4].id;
+    //   let elementToRemove = document.getElementById(lineItemId);
+    //   console.log(elementToRemove)
+    //     elementToRemove.remove();
+    // });
+  }
+
+  addHandler(id) {
     let productId = this.productTarget.value;
-    get(`/invoices/add_line_items?product_id=${productId}`, {
+    get(`/invoices/add_line_items?product_id=${id}`, {
+      responseKind: "turbo-stream"
+    });
+  }
+
+  removeHandler(id) {
+    console.log(id)
+    destroy(`/invoices/delete_line_items?line_item_id=${this.element.children[4].id}`, { 
       responseKind: "turbo-stream"
     });
   }
@@ -19,8 +57,9 @@ export default class extends Controller {
     this.handleLineItemChange()
   }
 
-  lineitemTargetDisconnected() {
-    this.handleLineItemChange()    
+  lineitemTargetDisconnected(event) {
+    this.handleLineItemChange() 
+    // this.handleItemRemove(event)   
   }
 
   handleLineItemChange(event) {
@@ -36,4 +75,14 @@ export default class extends Controller {
       responseKind: "turbo-stream"
     })
   }
+  
+  handleItemRemove(event) {
+    let selectedItems = this.select.items;
+    let deletedLineItem = event.children[4].value;
+    selectedItems.forEach(item => {
+        if (item === deletedLineItem) {
+            this.select.removeItem(item);
+        }
+    });
+  } 
 }
