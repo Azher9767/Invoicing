9c72560beb034abb2a83cec455ed83088cd9b327
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-04-10 18:13:31 +0530

    changes fixed

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,10 +1,10 @@
 require 'capybara/rspec'
 
-# Capybara.default_driver = :selenium
+Capybara.default_driver = :selenium
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = Selenium::WebDriver::Chrome::Options.new
 
-  # options.add_argument('--headless')
+  options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
   options.add_argument('--window-size=1280,800')
