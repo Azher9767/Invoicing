f27a4ccc84254329b51482717c31654d6de38ad8
Azher Shaikh <m.azhershaikh9767@gmail.com>
2024-04-29 17:49:22 +0530

    final chages fixed

--- spec/factories/tax_and_discount_polies.rb
+++ spec/factories/tax_and_discount_polies.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :tax_and_discount_poly do
-    amount { 0.0 }
+    amount { 18.0 }
     name { 'GST' }
     td_type { 'tax' }
   end
--- spec/services/invoice_amount_calculator_spec.rb
+++ spec/services/invoice_amount_calculator_spec.rb
@@ -1,19 +1,18 @@
 RSpec.describe InvoiceAmountCalculator do
   context "for line item" do
     let(:line_items) { build_list(:line_item, 2) }
-    let(:tax_and_discount_polies) { build_list(:tax_and_discount_poly, 1) }
     let(:expected_subtotal) { 200.0 }
 
     it "calculate subtotal for multiple line items" do
       sub_total_amount = InvoiceAmountCalculator.new
-      expect(sub_total_amount.calculate_sub_total(line_items, tax_and_discount_polies)).to eq(expected_subtotal)
+      expect(sub_total_amount.calculate_sub_total(line_items, [])).to eq(expected_subtotal)
     end
   end
 
   context "for line item with tax and discount" do
     let(:line_items) { build_list(:line_item, 2) }
-    let(:tax_and_discount_polies) { build_list(:tax_and_discount_poly, 1, name: "diwali offer", amount: -10, td_type: 'discount') }
-    let(:expected_subtotal) { 180.0 }
+    let(:tax_and_discount_polies) { build_list(:tax_and_discount_poly, 1) }
+    let(:expected_subtotal) { 236.0 }
 
     it "calculate subtotal for multiple line items" do
       sub_total_amount = InvoiceAmountCalculator.new
